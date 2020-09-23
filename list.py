dbcedb13b67ff6d46c5b2955dc8670e240346e8e
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-09-22 22:25:36 -0500

    Update list.py

--- list.py
+++ list.py
@@ -6,7 +6,8 @@ response=input("Enter 1 or 2; ")
 with open('To_Do_List.txt', 'r') as f:
   if response == 1: print(f.read())
   f.close()
-
+  
+##ERROR #if response == 2: with open('To_Do_List.txt', 'a') as f:#
 if response == 2: with open('To_Do_List.txt', 'a') as f:
    f.write(input("What would you like to add to your list?"))
    print(f.read())
