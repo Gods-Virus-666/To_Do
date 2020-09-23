e05ea39c1b76c0bc8a19424b2c5a7b8b72faf9d2
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-09-22 22:22:34 -0500

    Update list.py

--- list.py
+++ list.py
@@ -7,8 +7,7 @@ with open('To_Do_List.txt', 'r') as f:
   if response == 1: print(f.read())
   f.close()
 
-if response == 2: 
-  with open('To_Do_List.txt', 'a') as f:
+if response == 2: with open('To_Do_List.txt', 'a') as f:
    f.write(input("What would you like to add to your list?"))
    print(f.read())
    f.close()
