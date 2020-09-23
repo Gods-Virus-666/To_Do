d244bfab670f99b2d5f4e5f15bc63483dd3701fb
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-09-22 21:50:45 -0500

    Update list.py

--- list.py
+++ list.py
@@ -1,7 +1,8 @@
 print("Press 1 to view list or Press 2 to add to the list?")
 response=input("Enter 1 or 2; ")
-if response == 1 then print(list)
+if response == 1 then with open('To_Do_list.txt', r) as f and print(f.read())
+
 if response == 2 then 
-  with open('To_Do_List.txt', 'w') as f:
+  with open('To_Do_List.txt', 'a') as f:
    f.write(input("What would you like to add to your list?"))
-   
+   f.close
